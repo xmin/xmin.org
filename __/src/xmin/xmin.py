2160692fd58ed225ee979a78cf62b43ae680a002
Chad Whitacre <chad@zetaweb.com>
2010-10-01 19:20:59 -0700

    Changes from the field.

--- __/src/xmin/xmin.py
+++ __/src/xmin/xmin.py
@@ -3,7 +3,6 @@ import os
 
 import aspen
 import aspen.restarter
-import twitter
 from aspen.handlers.simplates import stdlib
 
 
@@ -11,26 +10,8 @@ TMPL = os.path.join(aspen.paths.root, '__', 'etc', 'template.html')
 aspen.restarter.track(TMPL)
 TMPL = open(TMPL).read()
 
-LINK = r"""<a title="Search #xmin on Twitter"
-              href="http://twitter.com/#search?q=%23xmin">\1</a>"""
-TWEET = """\
-    %(text)s
-    <span class="citation">&mdash;
-    <a title="Go to this tweet." 
-       "href="http://twitter.com/%(from_user)s/status/%(id)s">%(from_user)s</a>
-    </span>
-"""
-
-tag_re = re.compile(r'([#][Xx][Mm][Ii][Nn])')
-
-def get_tweet():
-    searcher = twitter.Twitter(domain="search.twitter.com")
-    result = searcher.search(q='#xmin')['results'][0]
-    result['text'] = tag_re.sub(LINK, result['text'])
-    return TWEET % result
 
 def webpage(environ, start_response):
-    tweet = get_tweet()
     orig = stdlib(environ, start_response)[0]
-    new = TMPL % (orig, tweet)
+    new = TMPL % orig
     return [new]
