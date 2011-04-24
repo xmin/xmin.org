49af6336761f2e2884910d49ea43d78c37494e0a
Chad Whitacre <chad@zetaweb.com>
2011-04-23 19:48:40 -0700

    Stripped it back down to "undefined, though not in so many words.

--- __/src/xmin/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup( name='xmin.org'
-     , version='0.1'
-     , author='Chad Whitacre'
-     , py_modules=['xmin']
-     , install_requires=['twitter == 1.4.2']
-      )
